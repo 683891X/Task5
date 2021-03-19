d4e569e14d47ca322355fc0309bc3759012714de
683891X <683891X@student.swin.edu.au>
2021-03-19 17:33:46 +1100

    Exercise two task two

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne;
+            int numberTwo;
+            int sum;
+
+            Console.WriteLine("Please enter the first number");
+            numberOne = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number");
+            numberTwo = int.Parse(Console.ReadLine());
+
+            sum = numberOne + numberTwo;
+
+            if(sum == 10){
+                Console.WriteLine("Sum of numbers is equal to ten");
+            }else if (sum > 10){
+                Console.WriteLine("Sum of numbers is greater than ten");
+            }else{
+                Console.WriteLine("Sum of numbers is less than ten");
+            }
+
+        }
+    }
+}
